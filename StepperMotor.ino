3004ca2f9211bb419f5f249e00954b63ae5dc7fb
Jacob Miske <jacobmiske@gmail.com>
2018-02-05 11:43:48 -0500

    Post Hackathon
    Part Files and Arduino Code

---
+++ StepperMotor.ino
@@ -0,0 +1,82 @@
+#include <Stepper.h>
+
+const int ledPin = 8;
+Stepper upStepper1(360, 0, 1, 2, 3);
+Stepper upStepper2(360, 0, 1, 2, 3);
+Stepper horizontalStepper(360, 0, 1, 2, 3);
+
+Stepper motionStepperLeft(360, 0, 1, 2, 3);
+Stepper motionStepperRight(360, 0, 1, 2, 3);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  
+  upStepper1.setSpeed(60);
+  upStepper2.setSpeed(60);
+  horizontalStepper.setSpeed(60);
+  motionStepperLeft.setSpeed(60);
+  motionStepperRight.setSpeed(60);
+}
+
+char buffer[255];
+
+void rotate() {
+  stepper.step(200);
+  delay(5000);
+  stepper.step(0);
+}
+
+void blink() {
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
+
+// Helper This is for the 90 degree turned gears
+void driveStep90(x, y, SteppMotor, Speed){
+  // Diameter is 25mm
+  Radius
+}
+
+// Helper This is for the straight on gears
+void driveStepStraight(x, y, SteppMotor, Speed){
+  // Diameter is 40mm
+  Radius = 20
+}
+
+int i = 0;
+void loop() {
+    if(Serial.available() > 0) {
+        char c = Serial.read();
+        if(c == '\n') {
+          buffer[i] = '\0';
+
+          String message(buffer);
+
+          if(message == "BLINK") {
+              blink();
+          } else if(message == "MOVE_FORWARD") {
+              move_forward();
+          } else if(message == "MOVE_BACKWARD") {
+              move_backward();
+          } else if(message == "MOVE_BAC") {
+              move_backward();
+          } 
+          i = 0;
+        } else {
+          buffer[i] = c;
+          i++;
+        }
+      }
+
+      int buttonState = digitalRead(buttonPin);
+      if(buttonState !=  lastButtonState) {
+        if(buttonState == LOW) {
+          Serial.println("Message1");
+        }
+        lastButtonState = buttonState;
+      }
+}
---
+++ raspberryHelper.ino
@@ -0,0 +1,128 @@
+private const int LED_PIN1 = 18;
+private const int LED_PIN2 = 23;
+private const int LED_PIN3 = 24;
+private const int LED_PIN4 = 25;
+private GpioPin pin1, pin2, pin3, pin4;
+
+private int iCounter = 0;
+
+public void Run(IBackgroundTaskInstance taskInstance)
+{
+    // Need to defer as a background application
+    deferral = taskInstance.GetDeferral();
+    InitGPIO();
+    stopwatch = Stopwatch.StartNew();
+
+    GpioController controller = GpioController.GetDefault();
+
+    timer = ThreadPoolTimer.CreatePeriodicTimer(this.Tick, TimeSpan.FromSeconds(2));
+
+    //You do not need to await this, as your goal is to have this run for the lifetime of the application
+    ThreadPool.RunAsync(this.MotorThread, WorkItemPriority.High);
+
+}
+
+// Init IOs - All must be low in initializing state
+private void InitGPIO()
+{
+    pin1 = GpioController.GetDefault().OpenPin(LED_PIN1);
+    pin1.Write(GpioPinValue.Low);
+    pin1.SetDriveMode(GpioPinDriveMode.Output);
+    pin2 = GpioController.GetDefault().OpenPin(LED_PIN2);
+    pin2.Write(GpioPinValue.Low);
+    pin2.SetDriveMode(GpioPinDriveMode.Output);
+    pin3 = GpioController.GetDefault().OpenPin(LED_PIN3);
+    pin3.Write(GpioPinValue.Low);
+    pin3.SetDriveMode(GpioPinDriveMode.Output);
+    pin4 = GpioController.GetDefault().OpenPin(LED_PIN4);
+    pin4.Write(GpioPinValue.Low);
+    pin4.SetDriveMode(GpioPinDriveMode.Output);
+
+}
+
+// You can change the direction here for every 2 seconds
+private void Tick(ThreadPoolTimer timer)
+{
+    
+}
+
+// 
+private void MotorThread(IAsyncAction action)
+{
+    //This motor thread runs on a high priority task and loops forever to pulse the motor
+    while (true)
+    {
+        switch (iCounter)
+        {
+            // 8 stats stepping
+            case 0:
+                pin1.Write(GpioPinValue.Low);
+                pin2.Write(GpioPinValue.Low);
+                pin3.Write(GpioPinValue.Low);
+                pin4.Write(GpioPinValue.High);
+                break;
+            case 1:
+                pin1.Write(GpioPinValue.Low);
+                pin2.Write(GpioPinValue.Low);
+                pin3.Write(GpioPinValue.High);
+                pin4.Write(GpioPinValue.High);
+                break;
+            case 2:
+                pin1.Write(GpioPinValue.Low);
+                pin2.Write(GpioPinValue.Low);
+                pin3.Write(GpioPinValue.High);
+                pin4.Write(GpioPinValue.Low);
+                break;
+            case 3:
+                pin1.Write(GpioPinValue.Low);
+                pin2.Write(GpioPinValue.High);
+                pin3.Write(GpioPinValue.High);
+                pin4.Write(GpioPinValue.Low);
+                break;
+            case 4:
+                pin1.Write(GpioPinValue.Low);
+                pin2.Write(GpioPinValue.High);
+                pin3.Write(GpioPinValue.Low);
+                pin4.Write(GpioPinValue.Low);
+                break;
+            case 5:
+                pin1.Write(GpioPinValue.High);
+                pin2.Write(GpioPinValue.High);
+                pin3.Write(GpioPinValue.Low);
+                pin4.Write(GpioPinValue.Low);
+                break;
+            case 6:
+                pin1.Write(GpioPinValue.High);
+                pin2.Write(GpioPinValue.Low);
+                pin3.Write(GpioPinValue.Low);
+                pin4.Write(GpioPinValue.Low);
+                break;
+            case 7:
+                pin1.Write(GpioPinValue.High);
+                pin2.Write(GpioPinValue.Low);
+                pin3.Write(GpioPinValue.Low);
+                pin4.Write(GpioPinValue.High);
+                break;
+            default: break;
+        }
+        if (iCounter == 7) iCounter = 0;
+        else iCounter++;
+        //Use the wait helper method to wait for the length of the pulse
+        Wait(1);
+    }
+}
+
+//A synchronous wait is used to avoid yielding the thread 
+//This method calculates the number of CPU ticks will elapse in the specified time and spins
+//in a loop until that threshold is hit. This allows for very precise timing.
+private void Wait(double milliseconds)
+{
+    long initialTick = stopwatch.ElapsedTicks;
+    long initialElapsed = stopwatch.ElapsedMilliseconds;
+    double desiredTicks = milliseconds / 1000.0 * Stopwatch.Frequency;
+    double finalTick = initialTick + desiredTicks;
+    while (stopwatch.ElapsedTicks < finalTick)
+    {
+
+    }
+}
